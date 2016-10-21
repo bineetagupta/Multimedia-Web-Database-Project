4a87751156db54ed873f65b48347d494c5deca08
Anurag Arora <anurag.arora@outlook.com>
2016-10-20 23:14:27 -0700

    correcting a mistake in creation of video and committing code for entropy based key frame extraction and its sample output for video 2R.mp4

--- Anurag/entropy_based_key_frame_extraction.m
+++ Anurag/entropy_based_key_frame_extraction.m
@@ -1,7 +1,7 @@
 % This program takes a video as input and extracts key frames from that
 % video based on euclidean distance between the frames.
 %
-% Below is a four (4) step process:
+% Below is a four (4 optional) step process:
 % 1. Calculating consecutive frame differences and storing them in an
 % matrix (distanceMatrix).
 % 2. Calculating a threshold for the distances to be compared against.
@@ -11,8 +11,7 @@
 % frames (keyFrameVideo).
 
 % Asking user to input values of r and n
-n=input('Give a value for n: ');
-arrKeyFrameInput=input('Do you want to make an array of Key Frames (computationally expensive) (y/n): ', 's');
+arrKeyFrameInput = input('Do you want to make a video out of Key Frames (computationally expensive) (y/n): ', 's');
 
 videoToLoad='sam_videos/2R.mp4';
 v=VideoReader(videoToLoad);
@@ -26,10 +25,10 @@ for frameIndex=1:(numberOfFrames-1)
     frame = rgb2gray(read(v, frameIndex));
     frameNext = rgb2gray(read(v, frameIndex+1));
     
-    histFrame1 = imhist(frame,n);
-    histFrame2 = imhist(frameNext,n);
+    histFrame1 = entropy(frame);
+    histFrame2 = entropy(frameNext);
     
-    d=pdist2(histFrame1',histFrame2');
+    d=sum(double(num2str(pdist2(histFrame1',histFrame2'))));
     distanceMatrix(frameIndex+1, 1)=d;
 end
 
@@ -47,18 +46,16 @@ for frameIndex=2: numberOfFrames
     end
 end
 
-%Converting key frames to a video which can be created back to a video if
+%Converting frame to a frame matrix which can be created back to a video if
 %needed
-if(arrKeyFrameInput=='y')
+if arrKeyFrameInput=='y'
     newFile = VideoWriter('newVideoFile');
     open(newFile);
     for frameIndex=1: numberOfFrames
         if(frameMatrix(frameIndex,2)==1)
             keyFrameVideo = im2frame(read(v, frameIndex));
-            writeVideo(newFile, keyFrameVideo);
         end
+        writeVideo(newFile, keyFrameVideo);
     end
     close(newFile);
 end
-
-
